f08ce90d992efacf6ab388a6a4a19374d1c5dfd3
Utente <Utente@10.169.221.28>
2017-05-10 20:15:54 +0200

    added the pages

--- app/views.py
+++ app/views.py
@@ -20,6 +20,8 @@ def page2():
 @app.route('/page3')
 def page3():
     user = {'nickname': 'User'}  # fake user
+    # here the idea is to show a loading page while the server compute the oufits and then re-render on 
+    # the same page the output page with the outfits
     return render_template('page3.html',
                            title='page3',
                            user=user)
