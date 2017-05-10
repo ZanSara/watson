72a1c80ca9de20a5d15e516b4f079584c851f0cc
Utente <Utente@10.169.221.28>
2017-05-10 16:24:32 +0200

    added pages and functions in views

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from time import sleep
 
 @app.route('/')
 @app.route('/index')
@@ -8,3 +9,18 @@ def index():
     return render_template('helloworld.html',
                            title='Home',
                            user=user)
+    
+
+@app.route('/page2')
+def page2():
+    user = {'nickname': 'User'}  # fake user
+    return render_template('page2.html',
+                           title='page2',
+                           user=user)
+@app.route('/page3')
+def page3():
+    user = {'nickname': 'User'}  # fake user
+    return render_template('page3.html',
+                           title='page3',
+                           user=user)
+
