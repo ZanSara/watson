5534bb7e3b2613e6269bdb5ed7bbb0fd46f765c8
IvanVigor <ivan.vigorito@mail.polimi.it>
2017-05-28 21:02:44 +0200

    filepicker support also 1000MB of photos now :P

--- app/static/js/inpage_filepicker.js
+++ app/static/js/inpage_filepicker.js
@@ -1,4 +1,62 @@
 
+function handleFiles()
+{
+    var filesToUpload = document.getElementById('input').file;
+    var file = filesToUpload;
+
+    // Create an image
+    var img = document.createElement("img");
+    // Create a file reader
+    var reader = new FileReader();
+    // Set the image once loaded into file reader
+    reader.onload = function(e)
+    {
+        img.src = e.target.result;
+
+        var canvas = document.createElement("canvas");
+        //var canvas = $("<canvas>", {"id":"testing"})[0];
+        var ctx = canvas.getContext("2d");
+        ctx.drawImage(img, 0, 0);
+
+        var MAX_WIDTH = 400;
+        var MAX_HEIGHT = 300;
+        var width = img.width;
+        var height = img.height;
+
+        if (width > height) {
+          if (width > MAX_WIDTH) {
+            height *= MAX_WIDTH / width;
+            width = MAX_WIDTH;
+          }
+        } else {
+          if (height > MAX_HEIGHT) {
+            width *= MAX_HEIGHT / height;
+            height = MAX_HEIGHT;
+          }
+        }
+        canvas.width = width;
+        canvas.height = height;
+        var ctx = canvas.getContext("2d");
+        ctx.drawImage(img, 0, 0, width, height);
+       
+        var imgData=ctx.getImageData(0, 0, width, height);
+        document.getElementById('input').file = imgData;
+        
+    }
+    // Load files into file reader
+    reader.readAsDataURL(file);
+
+
+    // Post the data
+    /*
+    var fd = new FormData();
+    fd.append("name", "some_filename.jpg");
+    fd.append("image", dataurl);
+    fd.append("info", "lah_de_dah");
+    */
+}
+
+
 var n=0;
 (function ( $ ) {
 	 
@@ -27,7 +85,7 @@ var n=0;
         var picker_btn_icon = $('<i class="'+settings.icon+'"></i>');
         
         // The actual file input which stays hidden
-        var picker_btn_input = $('<input id="'+n+'" type="file" name="'+settings.name+""+n+'" />');
+        var picker_btn_input = $('<input id="'+n+'" type="file" onchange="handleFiles()" name="'+settings.name+""+n+'" />');
         
         
         // The actual element displayed
