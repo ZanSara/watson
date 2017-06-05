22d4cfaca780426328661f623c91f7aa66cc17ae
Antonio <Antonio@192.168.1.112>
2017-06-05 22:04:23 +0200

    linked

--- config.py
+++ config.py
@@ -15,14 +15,14 @@ APP_TEMPLATE = os.path.join(APP_ROOT, 'templates')
 UPLOAD_FILE_PATH = "app/static/uploads/"
 UPLOAD_WEB_PATH = "../static/uploads/"
 
-FASHION_BLOGGER_FPATH = "app/static/dataset/collections/fashion_blogger/"
-FASHION_BLOGGER_WPATH = "../static/dataset/collections/fashion_blogger/"
+FASHION_BLOGGER_FPATH = "app/static/new_dataset/collections/fashion_blogger/"
+FASHION_BLOGGER_WPATH = "../static/new_dataset/collections/fashion_blogger/"
 
-LOWER_BODY_FPATH = "app/static/dataset/collections/lower_body/"
-LOWER_BODY_WPATH = "../static/dataset/collections/lower_body/"
+LOWER_BODY_FPATH = "app/static/new_dataset/collections/lower_body/"
+LOWER_BODY_WPATH = "../static/new_dataset/collections/lower_body/"
 
-UPPER_BODY_FPATH = "app/static/dataset/collections/upper_body/"
-UPPER_BODY_WPATH = "../static/dataset/collections/upper_body/"
+UPPER_BODY_FPATH = "app/static/new_dataset/collections/upper_body/"
+UPPER_BODY_WPATH = "../static/new_dataset/collections/upper_body/"
 
-FULL_BODY_FPATH = "app/static/dataset/collections/full_body/"
-FULL_BODY_WPATH = "../static/dataset/collections/full_body/"
+FULL_BODY_FPATH = "app/static/new_dataset/collections/full_body/"
+FULL_BODY_WPATH = "../static/new_dataset/collections/full_body/"
